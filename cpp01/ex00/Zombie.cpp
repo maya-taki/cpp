41311deb253d87aa6d90f1c0d7df6815dd7c315e
maya-taki <g.takiyoshi@gmail.com>
2026-09-14 20:45:23 -0300

    BUILD: refactor Zombie class methods and update file timestamps

--- cpp01/ex00/Zombie.cpp
+++ cpp01/ex00/Zombie.cpp
@@ -6,11 +6,16 @@
 /*   By: mtakiyos <mtakiyos@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/13 17:49:47 by mtakiyos          #+#    #+#             */
-/*   Updated: 2026/09/13 18:14:04 by mtakiyos         ###   ########.fr       */
+/*   Updated: 2026/09/14 15:09:20 by mtakiyos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
+
+void	Zombie::announce(void)
+{
+	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
 
 Zombie::Zombie(std::string &name)
 {
@@ -19,10 +24,5 @@ Zombie::Zombie(std::string &name)
 
 Zombie::~Zombie()
 {
-	std::cout << name << " has died" << std::endl;
-}
-
-void	Zombie::announce(void)
-{
-	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << name << ": died" << std::endl;
 }
